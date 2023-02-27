e777b0287aaa5be0c678bda67ef92b06cb4c7d87
srikarthickmuthu <srikarthick.muthu@aspiresys.com>
2023-02-27 18:34:17 +0530

    Arrow function and exports

---
+++ function1.js
@@ -0,0 +1,17 @@
+function sum(a,b){
+    return a+b;
+}
+
+//arrow function
+
+const multiple = (a,b)=>{
+
+    return a*b;
+    
+}
+
+module.exports={
+    sum , multiple
+};
+
+console.log("process",process)
---
+++ functions.js
@@ -0,0 +1,22 @@
+
+//variable var let const
+
+
+//imports 
+
+
+const helpers=require("./function1");
+
+const total = helpers.sum(4,5);
+
+console.log("Total :",total);
+
+const multiple=helpers.multiple(5,9);
+
+console.log("arrow_multiple",multiple);
+
+//using object destructuring
+
+const { sum }=require("./function1")
+
+console.log("Object destruturing Sum :" ,sum(10,20));
